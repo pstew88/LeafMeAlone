4ce843e696d7821f05894fc6cc17536fc8aed13c
Pierce Steward <pierce.steward@gmail.com>
2021-04-28 16:24:20 -0600

    poroject stub

---
+++ LeafMeAlone/src/main/java/com/leaf/LeafMeAloneApplication.java
@@ -0,0 +1,13 @@
+package com.leaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafMeAloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafMeAloneApplication.class, args);
+	}
+
+}
---
+++ LeafMeAlone/src/test/java/com/leaf/LeafMeAloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafMeAloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
